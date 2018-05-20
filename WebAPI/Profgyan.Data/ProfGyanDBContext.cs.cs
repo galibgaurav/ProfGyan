c3081a1102fbb941854c8cedf17ada43cef1ee0f
Galib Gaurav <GalibGaurav@gmail.com>
2018-05-20 14:49:55 +0530

    Initial commit

---
+++ WebAPI/Profgyan.Data/ProfGyanDBContext.cs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+
+namespace Profgyan.Data
+{
+    public class ProfGyanUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+        //    // Add custom user claims here
+        //    return userIdentity;
+        //}
+    }
+
+    public class ProfGyanDBContext : IdentityDbContext<ProfGyanUser>
+    {
+        public ProfGyanDBContext()
+            : base("Data Source=IN-L20042\\SQLSERVER2014;Database=ProfGyanDB;Integrated Security=False;User ID=sa;Password=Password1;MultipleActiveResultSets=true;", throwIfV1Schema: false)
+        {
+        }
+
+        //public ProfGyanDBContext()
+        //    : base("Data Source=IN-L20042\\SQLSERVER2014;Database=ProfGyanDB;Integrated Security=False;User ID=sa;Password=Password1;MultipleActiveResultSets=true;", throwIfV1Schema: false)
+        //{
+        //} 
+        public static ProfGyanDBContext Create()
+        {
+            return new ProfGyanDBContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //AspNetUsers -> User
+            modelBuilder.Entity<ProfGyanUser>()
+                .ToTable("User");
+            //AspNetRoles -> Role
+            modelBuilder.Entity<IdentityRole>()
+                .ToTable("Role");
+            //AspNetUserRoles -> UserRole
+            modelBuilder.Entity<IdentityUserRole>()
+                .ToTable("UserRole");
+            //AspNetUserClaims -> UserClaim
+            modelBuilder.Entity<IdentityUserClaim>()
+                .ToTable("UserClaim");
+            //AspNetUserLogins -> UserLogin
+            modelBuilder.Entity<IdentityUserLogin>()
+                .ToTable("UserLogin");
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Profgyan.Data;
+using Profgyan.Model;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProfGyan.Controllers
+{
+    public class AccountController : ApiController
+    {
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("api/User/Register")]
+        [HttpPost]
+        public async Task<IdentityResult> Register(RegisterBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new IdentityResult("Registraion Data not valid");
+                //return BadRequest(ModelState);
+            }
+            var userStore = new UserStore<ProfGyanUser>(new ProfGyanDBContext());
+            var manager = new UserManager<ProfGyanUser>(userStore);
+            var user = new ProfGyanUser() { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+
+            IdentityResult result = await manager.CreateAsync(user, model.Password);
+            return result;
+
+        }
+
+        [HttpGet]
+        [Route("api/GetUserClaims")]
+        [Authorize]
+        //Only for testing the Auth and auth feature
+        public RegisterBindingModel GetUserClaims()
+        {
+            var identityClaims = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identityClaims.Claims;
+            RegisterBindingModel model = new RegisterBindingModel()
+            {
+                UserName = identityClaims.FindFirst("Username").Value,
+                Email = identityClaims.FindFirst("Email").Value,
+                FirstName = identityClaims.FindFirst("FirstName").Value,
+                LastName = identityClaims.FindFirst("LastName").Value,
+                LoggedOn = identityClaims.FindFirst("LoggedOn").Value
+            };
+            return model;
+        }
+
+    }
+}
